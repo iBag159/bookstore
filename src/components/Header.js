7c2790260851a7d9802cbc07702718f28d45900e
Gaby Alvarez <galvarezc@unitru.edu.pe>
2021-01-02 15:31:53 -0500

    Main structure - index.js

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return <nav>
+            <ul>Book Store</ul>
+            <ul>
+                <li>Home</li>
+                <li>Shop</li>
+                <li>Blog</li>
+                <li>Portfolio</li>
+                <li>Author</li>
+                <li>Contact</li>
+            </ul>
+            <ul></ul>
+        </nav>
+    }
+}
+
+export default Header;
